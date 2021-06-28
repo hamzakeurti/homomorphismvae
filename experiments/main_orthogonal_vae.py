d7fb7aa10ac787b3d16687f99f9be4f2e721c735
Hamza Keurti <hamza.keurti@gmail.com>
2021-06-28 17:13:28 +0200

    minor changes

--- experiments/main_orthogonal_vae.py
+++ experiments/main_orthogonal_vae.py
@@ -1,11 +1,8 @@
 import torch
 import torch.nn as nn
-from torch import optim
 
 
 import os
-import sys
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import __init__
 from experiments import train_utils
