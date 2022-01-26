4dda56197f7a58dd351563283bd3ac0f1e63e6cb
Hamza Keurti <hamza.keurti@gmail.com>
2022-01-26 17:25:50 +0100

    rotated mnist dataset first commit

---
+++ displacementae/data/mnist.py
@@ -0,0 +1,68 @@
+import os
+import numpy as np
+import torch
+import torchvision
+from torch.utils.data import Dataset
+from skimage.transform import rotate
+
+class MNISTDataset(Dataset):
+    def __init__(self, root, rseed=None, fixed_in_sampling=[], 
+                fixed_values=[], fixed_in_intervention=[], intervene=True,
+                angles_range=[-180,180], intervention_range=[-6,6], 
+                num_train = 200, num_val=30, 
+                num_digits = 100):
+        super().__init__()
+        self.dataset = torchvision.datasets.MNIST(
+            root= root, train = True, download = True)
+        
+        self.intervene = intervene
+
+        # Random generator
+        if rseed is not None:
+            rand = np.random.RandomState(rseed)
+        else:
+            rand = np.random
+
+        # Number of samples
+        self.num_train = num_train
+        self.num_val = num_val
+
+        # Number of digits
+        self.num_digits = num_digits
+
+        # Sample digits
+        self.digits_ids = rand.randint(len(self.dataset),size=self.num_digits)
+
+        # Sample train set
+        # Sample angles
+        self.train_digits = rand.choice(self.digits_ids,size=self.num_train)
+        self.train_angles_1 = rand.uniform(
+            low=angles_range[0],high=angles_range[1],size=self.num_train)
+        if self.intervene:
+            self.train_angles_2 = rand.uniform(
+                low=intervention_range[0],high=intervention_range[1],
+                size=self.num_train)
+            self.train_angles_2 += self.train_angles_1
+
+        data = {}
+        data["in_shape"] = [1,28,28]
+        data["action_shape"] = [int(intervene)]
+        self._data = data
+
+    def __len__(self):
+        return self.num_train
+
+    def __getitem__(self, index):
+        angle1 = self.train_angles_1[index]
+        idx = self.train_digits[index]
+        img, label = self.dataset[idx]
+        img1 = rotate(np.array(img),angle1)
+        label1 = np.array([angle1,label])
+        if self.intervene:
+            angle2 = self.train_angles_2[index]
+            img2 = rotate(np.array(img),angle2)
+            label2 = np.array([angle2,label])
+            dj = angle2-angle1
+            return img1,label1,img2,label2,dj
+        else:
+            return img1,label1,img1,label1,0
