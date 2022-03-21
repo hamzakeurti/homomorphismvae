061aef3d1b885e0cc1b17f39ab671621e5dfd65e
Hamza Keurti <hamza.keurti@gmail.com>
2022-03-21 18:10:02 +0100

    Group Representation interface

---
+++ displacementae/grouprepr/group_representation.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Copyright 2022 Hamza Keurti
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# @title          :displacementae/grouprepr/group_representation.py
+# @author         :Hamza Keurti
+# @contact        :hkeurti@ethz.ch
+# @created        :21/03/2022
+# @version        :1.0
+# @python_version :3.7.4
+
+
+
+
+
+import torch.nn as nn
+
+class GroupRepresentation(nn.Module):
+    """
+    An interface for group representation classes.
+
+    Group representations map input transitions/actions to matrices.
+    """
+    def __init__(self, n_input, n_repr_units) -> None:
+        super().__init__()
+        self.n_input = n_input
+        self.n_repr_units = n_repr_units
+
+    def get_matrix(self,a):
+        """
+        Maps input transitions to matrices.
+        """
+        pass
+
+    def compose(self,a):
+        """
+        Maps a succession of transitions to a matrix by calculating the matrix 
+        product of the matrix images of the input actions.
+
+        Args:
+            a, torch.Tensor: Batch of succesion of transitions. 
+                        shape: `[batch_size,n_steps,n_input]` 
+        Returns:
+            torch.Tensor: Batch of matrices.
+                        shape: `[batch_size,n_repr_units,n_repr_units]`
+        """
+        return
