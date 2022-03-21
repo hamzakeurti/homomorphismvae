ced9f8f6d61f896af3e295007ab2fb94900c28a0
Hamza Keurti <hamza.keurti@gmail.com>
2022-03-21 17:38:45 +0100

    Multistep autoencoder

---
+++ displacementae/networks/multistep_autoencoder.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# Copyright 2021 Hamza Keurti
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# @title          :displacementae/networks/multistep_autoencoder.py
+# @author         :Hamza Keurti
+# @contact        :hkeurti@ethz.ch
+# @created        :21/03/2022
+# @version        :1.0
+# @python_version :3.7.4
+
+import torch
+
+import networks.autoencoder as ae
+
+class MultistepAutoencoder(ae.AutoEncoder):
+    def __init__(self, encoder, decoder, grp_morphism, n_repr_units, 
+                 n_transform_units, variational=True, spherical=False, 
+                 n_steps = 1):
+        """
+        An Autoencoder with multiple future observation prediction through 
+        group representation.
+
+        encoder, nn.Module: Encoder Network. Maps inputs to a representation 
+                        vector or to parameters of a posterior distribution 
+                        in the variational case. 
+        decoder, nn.Module: Decoder Network. Maps a representation 
+                        vector back in the input space. 
+        grp_transformation, nn.Module: Maps an action to a transformation of
+                        the representation space.
+        variational, bool: If True, the encoder describes a distribution instead 
+                        of being deterministic, defaults to True.
+        spherical, bool: If True, the encoder's outputs (the location part 
+                        in the variational case) is normalized.
+        n_steps, int: Number of observed transitions. Transitions are mapped to
+                        matrices through the group morphism 
+        """
+        super().__init__(encoder=encoder,decoder=decoder,
+                         grp_morphism=grp_morphism, n_repr_units=n_repr_units,
+                         variational=variational, spherical=spherical)
+        self.n_transform_units = n_transform_units
+        self.n_steps = n_steps
+
+
+    def forward(self, x, dz):
+        """
+        Encodes the input image and predicts the `n_steps` following images.
+
+        Encodes the input image `x`. Decodes each image after the ith observed 
+        transition. Transitions `dz` are mapped to matrices through the 
+        `grp_morphism`, matrices are applied to the obtained representation.
+        """
+        h = x
+    
+        # Through encoder
+        h, mu, logvar = self.encode(h)
+
+        h_out = torch.empty(
+            size=[x.shape[0] + [self.n_steps, self.n_repr_units]])
+
+        out = torch.empty(
+            size=[x.shape[0] + [self.n_steps] + list(x.shape[1:])])
+
+        if self.n_repr_units > self.n_transform_units:
+            # The part of the transformation that is not transformed 
+            # is repeated for all transition steps.
+            h_out[:,:,self.n_transform_units:] = \
+                                    h.unsqueeze(1).repeat(1,self.n_steps,1)
+        # Through geometry
+        for i in range(self.n_steps):
+            h_out[:,:,:self.n_transform_units] = \
+                self.grp_morphism.transform( h[:,:self.n_transform_units], 
+                                        dz[:,:i+1])
+                
+        # Through decoder
+        h_out = h_out.view(-1, self.n_repr_units)
+        h_out = self.decoder(h_out)
+        h_out = h_out.view(x.shape[0],self.n_steps,*x.shape[1:])
+        if self.variational:
+            return h_out, mu, logvar
+        else:
+            return h_out, None, None
+
