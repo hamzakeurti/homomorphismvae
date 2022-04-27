a99ba602c856557150f4b8d6c6ba2a679e793a7c
Hamza Keurti <hamza.keurti@gmail.com>
2022-04-27 15:08:57 +0000

    Normalize representations when plotting manifolds

--- displacementae/utils/plotting_utils.py
+++ displacementae/utils/plotting_utils.py
@@ -172,6 +172,7 @@ def plot_manifold(dhandler, nets, shared, config, device, logger, mode,
         X = torch.FloatTensor(images).to(device)
         with torch.no_grad():
             h, mu, logvar = nets.encode(X)
+            h = nets.normalize_representation(h)
             results.append(h[:,plot_latent].cpu().numpy())
     results = np.vstack(results).squeeze()
     
@@ -245,6 +246,7 @@ def plot_manifold_pca(dhandler, nets, shared, config, device, logger, mode,
         X = torch.FloatTensor(images).to(device)
         with torch.no_grad():
             h, mu, logvar = nets.encode(X)
+            h = nets.normalize_representation(h)
             results.append(h[:,:].cpu().numpy())
     results = np.vstack(results).squeeze()
 
